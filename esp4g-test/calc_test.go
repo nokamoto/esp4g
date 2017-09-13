916558d87e40594bee753f8a20994a2d2d06fdb6
nokamoto <nokamoto@hotmail.co.jp>
2017-09-13 16:41:59 +0900

    Lock test variables

--- esp4g-test/calc_test.go
+++ esp4g-test/calc_test.go
@@ -3,20 +3,62 @@ package esp4g_test
 import (
 	calc "github.com/nokamoto/esp4g/examples/calc/protobuf"
 	"io"
+	"sync"
 )
 
 type CalcService struct {
-	lastAllRequests       []*calc.Operand
-	lastAllResponse       *calc.Sum
-	lastDefferedRequest   *calc.OperandList
-	lastDefferedResponses []*calc.Sum
-	lastAsyncRequests     []*calc.Operand
-	lastAsyncResponses    []*calc.Sum
+	_lastAllRequests       []*calc.Operand
+	_lastAllResponse       *calc.Sum
+	_lastDefferedRequest   *calc.OperandList
+	_lastDefferedResponses []*calc.Sum
+	_lastAsyncRequests     []*calc.Operand
+	_lastAsyncResponses    []*calc.Sum
+
+	mu *sync.Mutex
+}
+
+func (c *CalcService)lastAllRequests() []*calc.Operand {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c._lastAllRequests
+}
+
+func (c *CalcService)lastAllResponse() *calc.Sum {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c._lastAllResponse
+}
+
+func (c *CalcService)lastDefferedRequest() *calc.OperandList {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c._lastDefferedRequest
+}
+
+func (c *CalcService)lastDefferedResponses() []*calc.Sum {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c._lastDefferedResponses
+}
+
+func (c *CalcService)lastAsyncRequests() []*calc.Operand {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c._lastAsyncRequests
+}
+
+func (c *CalcService)lastAsyncResponses() []*calc.Sum {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c._lastAsyncResponses
 }
 
 func (c *CalcService)AddAll(stream calc.CalcService_AddAllServer) error {
-	c.lastAllRequests = []*calc.Operand{}
-	c.lastAllResponse = &calc.Sum{}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c._lastAllRequests = []*calc.Operand{}
+	c._lastAllResponse = &calc.Sum{}
 
 	for {
 		req, err := stream.Recv()
@@ -26,11 +68,11 @@ func (c *CalcService)AddAll(stream calc.CalcService_AddAllServer) error {
 		if err != nil {
 			return err
 		}
-		c.lastAllRequests = append(c.lastAllRequests, req)
-		c.lastAllResponse.Z = c.lastAllResponse.Z + req.GetX() + req.GetY()
+		c._lastAllRequests = append(c._lastAllRequests, req)
+		c._lastAllResponse.Z = c._lastAllResponse.Z + req.GetX() + req.GetY()
 	}
 
-	if err := stream.SendAndClose(c.lastAllResponse); err != nil {
+	if err := stream.SendAndClose(c._lastAllResponse); err != nil {
 		return err
 	}
 
@@ -38,24 +80,30 @@ func (c *CalcService)AddAll(stream calc.CalcService_AddAllServer) error {
 }
 
 func (c *CalcService)AddDeffered(req *calc.OperandList, stream calc.CalcService_AddDefferedServer) error {
-	c.lastDefferedResponses = []*calc.Sum{}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c._lastDefferedResponses = []*calc.Sum{}
 
 	for _, operand := range req.GetOperand() {
 		sum := &calc.Sum{Z: operand.GetX() + operand.GetY()}
 		if err := stream.Send(sum); err != nil {
 			return err
 		}
-		c.lastDefferedResponses = append(c.lastDefferedResponses, sum)
+		c._lastDefferedResponses = append(c._lastDefferedResponses, sum)
 	}
 
-	c.lastDefferedRequest = req
+	c._lastDefferedRequest = req
 
 	return nil
 }
 
 func (c *CalcService)AddAsync(stream calc.CalcService_AddAsyncServer) error {
-	c.lastAsyncRequests = []*calc.Operand{}
-	c.lastAsyncResponses = []*calc.Sum{}
+	c.mu.Lock()
+	defer c.mu.Unlock()
+
+	c._lastAsyncRequests = []*calc.Operand{}
+	c._lastAsyncResponses = []*calc.Sum{}
 
 	for {
 		operand, err := stream.Recv()
@@ -71,8 +119,8 @@ func (c *CalcService)AddAsync(stream calc.CalcService_AddAsyncServer) error {
 			return err
 		}
 
-		c.lastAsyncRequests = append(c.lastAsyncRequests, operand)
-		c.lastAsyncResponses = append(c.lastAsyncResponses, sum)
+		c._lastAsyncRequests = append(c._lastAsyncRequests, operand)
+		c._lastAsyncResponses = append(c._lastAsyncResponses, sum)
 	}
 
 	return nil
