6c706ae039fead35e15f5e2270f80cd8c93fb586
nokamoto <nokamoto@hotmail.co.jp>
2017-09-14 20:12:34 +0900

    Update docker compose files

--- docker/check-docker-compose.sh
+++ docker/check-docker-compose.sh
@@ -12,6 +12,11 @@ check () {
         docker-compose exec $service echo ok
     done
 
+    if [ "$2" != "" ]
+    then
+        $2 -test
+    fi
+
     docker-compose stop
 }
 
@@ -20,10 +25,10 @@ set -ex
 cd `dirname $0`
 
 cd ../examples/ping
-check ping
+check ping esp4g-ping
 
 cd ../calc
-check calc
+check calc esp4g-calc
 
 cd ../benchmark
 check benchmark
